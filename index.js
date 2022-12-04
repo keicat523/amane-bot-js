84db1c81a811ec7c68837c76a74e9c48fe11e57e
keicat523 <119162872+keicat523@users.noreply.github.com>
2022-12-04 20:07:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ let id_jrole = "1048569758364205066";
 //"1048854190564323388"; にとねころーる
 //"1048569758364205066"; あまねころーる
 
+
 client.on("ready", async () => {
   console.log(`${client.user.tag} でログインしています。`);
   const data = [{
     name: "参拝",
-    description: "月島神社へようこそ.....",
+    description: "月雲神社へようこそ.....",
   }];
-  await client.application.commands.set(data, '1043408170330107914');
+  await client.application.commands.set(data, '1048567387085082706');
+  client.user.setActivity("月雲神社の掃除をプレイ中｜ver1.0｜ ⠀⠀⠀⠀⠀⠀⠀")
 
 });
 client.on("interactionCreate", async (interaction) => {
@@ -24,7 +26,7 @@ client.on("interactionCreate", async (interaction) => {
   }
   if (interaction.commandName === '参拝') {
     if (interaction.member.roles.cache.has(id_jrole) === false) {
-      interaction.reply("<@!" + interaction.member.id + "> さん。ようこそ月島神社へ。");
+      interaction.reply("<@!" + interaction.member.id + "> さん。ようこそ月雲神社へ。");
       const join = await interaction.member.roles.add(id_jrole)
       await setTimeout(3000);
       await interaction.deleteReply()
@@ -40,7 +42,7 @@ client.on("messageCreate", async msg => {
 
   if (msg.content === "z.参拝" & msg.member.user.bot != true) {
     if (msg.member.roles.cache.has(id_jrole) === false) {
-      const join = await msg.channel.send("<@!" + msg.member.id + "> さん。ようこそ月島神社へ。");
+      const join = await msg.channel.send("<@!" + msg.member.id + "> さん。ようこそ月雲神社へ。");
       await msg.member.roles.add(id_jrole)
       await setTimeout(3000);
       await join.delete()
@@ -53,6 +55,4 @@ client.on("messageCreate", async msg => {
   }
 }
 );
-//MTA0ODU4NzU5ODMxNjE4MzU4Mw.GFSvkM._rcReZl2iQSm5HraDYUu3-3YMykgBZIaFRrsuY あまね
-//MTAzMzE4MDczNzM2NjY3MTM4MA.GOCiQO.2JnoCGPPMeMoZD025WFGX2Ptk3GNb71wGKok5o nitro
-client.login("MTA0ODU4NzU5ODMxNjE4MzU4Mw.GFSvkM._rcReZl2iQSm5HraDYUu3-3YMykgBZIaFRrsuY");
+client.login(BOT_TOKEN);
